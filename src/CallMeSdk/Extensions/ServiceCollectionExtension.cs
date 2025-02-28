9f72b93222408b6e27905c5129199ddace49f363
eser.sahin <eser.sahin@pyrotech.com.tr>
2025-02-28 22:09:23 +0300

    Method chaining implemented.

--- src/CallMeSdk/Extensions/ServiceCollectionExtension.cs
+++ src/CallMeSdk/Extensions/ServiceCollectionExtension.cs
@@ -8,19 +8,19 @@ public static class ServiceCollectionExtension
 
         services.AddSingleton<IDataProviderFactory, DataProviderFactory>();
         
-        services.AddTransient<IDataProvider<SoapConfiguration>, SoapDataProvider>();
-        services.AddTransient<IDataProvider<RestConfiguration>, RestDataProvider>();
-        services.AddTransient<IDataProvider<FtpConfiguration>, FtpDataProvider>();
+        services.AddTransient<IDataProvider<SoapConfiguration>, SoapDataProvider>().
+            AddTransient<IDataProvider<RestConfiguration>, RestDataProvider>().
+            AddTransient<IDataProvider<FtpConfiguration>, FtpDataProvider>();
 
-        services.AddSingleton<ICustomerIdService, CustomerIdService>();
-        services.AddTransient<ICustomerService, CustomerService>();
+        services.AddSingleton<ICustomerIdService, CustomerIdService>().
+            AddTransient<ICustomerService, CustomerService>();
 
-        services.AddTransient<ICompositeDataProvider, CompositeDataProvider>();
-        services.AddTransient(typeof(ITimingDataProvider<>), typeof(TimingDataProvider<>));
+        services.AddTransient<ICompositeDataProvider, CompositeDataProvider>().
+            AddTransient(typeof(ITimingDataProvider<>), typeof(TimingDataProvider<>));
 
-        services.AddKeyedTransient<IFtpClient, FluentFtpClient>(FtpClientType.FluentFtp.GetKey());
-        services.AddSingleton<IFtpClientConfigurator, FtpClientConfigurator>();
-        services.AddSingleton<IFtpClientFactory, FtpClientFactory>();
+        services.AddSingleton<IFtpClientConfigurator, FtpClientConfigurator>().
+            AddSingleton<IFtpClientFactory, FtpClientFactory>().
+            AddKeyedTransient<IFtpClient, FluentFtpClient>(FtpClientType.FluentFtp.GetKey());
         
         services.AddScoped<JsonSerializerOptions>(provider =>
         {
